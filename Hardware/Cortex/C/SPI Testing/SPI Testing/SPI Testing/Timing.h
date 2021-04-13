9d2eba69ac4caba40adce1c0b468061a8e3e232d
ajhanley <72047867+ajhanley@users.noreply.github.com>
2021-04-13 00:10:10 -0400

    Added Intital Testing for Encoder Based Stall Detection

--- Hardware/Cortex/C/SPI Testing/SPI Testing/SPI Testing/Timing.h
+++ Hardware/Cortex/C/SPI Testing/SPI Testing/SPI Testing/Timing.h
@@ -8,40 +8,46 @@
 #ifndef TIMING_H_
 #define TIMING_H_
 void changeClock(){
+	/* Set the correct number of wait states for 48 MHz @ 3.3v */
 	NVMCTRL->CTRLB.bit.RWS = 1;
-	SYSCTRL->XOSC32K.reg =SYSCTRL_XOSC32K_STARTUP(0x7) |SYSCTRL_XOSC32K_EN32K |SYSCTRL_XOSC32K_XTALEN;
-	SYSCTRL->XOSC32K.bit.ENABLE = 1;
-	while(!SYSCTRL->PCLKSR.bit.XOSC32KRDY);
-	
-	GCLK->GENDIV.reg =GCLK_GENDIV_ID(1) |GCLK_GENDIV_DIV(1);
-	GCLK->GENCTRL.reg =
-	GCLK_GENCTRL_ID(1) |GCLK_GENCTRL_SRC_XOSC32K |GCLK_GENCTRL_IDC |GCLK_GENCTRL_GENEN;
-	while(GCLK->STATUS.bit.SYNCBUSY);
-	
-	GCLK->CLKCTRL.reg =GCLK_CLKCTRL_ID_DFLL48 |GCLK_CLKCTRL_GEN_GCLK1 |GCLK_CLKCTRL_CLKEN;
-	
+	/* This works around a quirk in the hardware (errata 1.2.1) -
+	   the DFLLCTRL register must be manually reset to this value before
+	   configuration. */
 	while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
 	SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE;
 	while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
 
+	/* Write the coarse and fine calibration from NVM. */
+	uint32_t coarse =
+		((*(uint32_t*)FUSES_DFLL48M_COARSE_CAL_ADDR) & FUSES_DFLL48M_COARSE_CAL_Msk) >> FUSES_DFLL48M_COARSE_CAL_Pos;
+	uint32_t fine =
+		((*(uint32_t*)FUSES_DFLL48M_FINE_CAL_ADDR) & FUSES_DFLL48M_FINE_CAL_Msk) >> FUSES_DFLL48M_FINE_CAL_Pos;
 
-	SYSCTRL->DFLLMUL.reg =SYSCTRL_DFLLMUL_MUL(1465) |SYSCTRL_DFLLMUL_FSTEP(511) |SYSCTRL_DFLLMUL_CSTEP(31);
-	while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
-	
-	uint32_t coarse = (*((uint32_t *)FUSES_DFLL48M_COARSE_CAL_ADDR) & FUSES_DFLL48M_COARSE_CAL_Msk) >> FUSES_DFLL48M_COARSE_CAL_Pos;
-	SYSCTRL->DFLLVAL.bit.COARSE = coarse;
-	while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
-	
-	SYSCTRL->DFLLCTRL.reg |=SYSCTRL_DFLLCTRL_MODE |SYSCTRL_DFLLCTRL_WAITLOCK |SYSCTRL_DFLLCTRL_ENABLE;
-	while (!SYSCTRL->PCLKSR.bit.DFLLLCKC || !SYSCTRL->PCLKSR.bit.DFLLLCKF) {}
+	SYSCTRL->DFLLVAL.reg = SYSCTRL_DFLLVAL_COARSE(coarse) | SYSCTRL_DFLLVAL_FINE(fine);
+
+	/* Wait for the write to finish. */
+	while (!SYSCTRL->PCLKSR.bit.DFLLRDY) {};
+	/* Enable the DFLL */
+	SYSCTRL->DFLLCTRL.bit.ENABLE = 1;
+
+	/* Wait for the write to finish */
+	while (!SYSCTRL->PCLKSR.bit.DFLLRDY) {};
 		
-	GCLK->GENCTRL.reg =GCLK_GENCTRL_ID(0) |GCLK_GENCTRL_SRC_DFLL48M |GCLK_GENCTRL_IDC |GCLK_GENCTRL_GENEN;
+	/* Setup GCLK0 using the DFLL @ 48 MHz */
+	GCLK->GENCTRL.reg =
+		GCLK_GENCTRL_ID(0) |
+		GCLK_GENCTRL_SRC_DFLL48M |
+		/* Improve the duty cycle. */
+		GCLK_GENCTRL_IDC |
+		GCLK_GENCTRL_GENEN;
+
+	/* Wait for the write to complete */
 	while(GCLK->STATUS.bit.SYNCBUSY);
 }
 void initRTC(){
 	//A generic clock (GCLK_RTC) is required to clock the RTC. This clock must be configured and enabled in the Generic Clock Controller before using the RTC(Gen Clock ID 4, and )
 	
-	GCLK->GENDIV.reg  = GCLK_GENDIV_ID(0)|GCLK_GENDIV_DIV(0);
+	GCLK->GENDIV.reg  = GCLK_GENDIV_ID(0)|GCLK_GENDIV_DIV(6);
 	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID_RTC | GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN(0);
 	
 	/*GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(4)|  GCLK_GENCTRL_GENEN;
@@ -51,7 +57,7 @@ void initRTC(){
 
 	RTC->MODE0.CTRL.reg = 0x0;
 	//RTC->MODE0.CTRL.reg |= RTC_MODE0_CTRL_MATCHCLR;
-	RTC->MODE0.INTENSET.reg |= RTC_MODE0_INTENSET_CMP(8);
+	RTC->MODE0.INTENSET.reg |= RTC_MODE0_INTENSET_CMP(48);
 	//RTC->MODE0.EVCTRL.reg |= RTC_MODE0_EVCTRL_CMPEO(48);
 	RTC->MODE0.CTRL.reg |= RTC_MODE0_CTRL_ENABLE;
 	
@@ -59,8 +65,8 @@ void initRTC(){
 void delay_us(int num){
 	for(int i = num; i>0; i--){
 		RTC->MODE0.COMP->reg=0;
-		RTC->MODE0.INTFLAG.reg = RTC_MODE0_INTFLAG_CMP(8);
-		while(!RTC->MODE0.INTFLAG.reg && RTC_MODE0_INTFLAG_CMP(8));
+		RTC->MODE0.INTFLAG.reg = RTC_MODE0_INTFLAG_CMP(48);
+		while(!RTC->MODE0.INTFLAG.reg && RTC_MODE0_INTFLAG_CMP(48));
 	}
 	
 }
--- Hardware/Cortex/C/SPI Testing/SPI Testing/SPI Testing/main.c
+++ Hardware/Cortex/C/SPI Testing/SPI Testing/SPI Testing/main.c
@@ -27,16 +27,16 @@
 #define STEP &PA14
 #define DIRPIN &PA15
 volatile uint8_t recv[9][5] = {{0x00,0x00,0x00,0x00,0x00},{0x00,0x00,0x00,0x00,0x00},{0x00,0x00,0x00,0x00,0x00},{0x00,0x00,0x00,0x00,0x00},{0x00,0x00,0x00,0x00,0x00}};
-const uint8_t required[9][5] ={{0xEC,0x01,0x01,0x00,0xC2},{0x90,0x00,0x06,0x1F,0x0A},{0x91,0x00,0x00,0x00,0x0A},{0x80,0x00,0x00,0x00,0x03},{0x93,0x00,0x00,0x00,0x00},{0x6F,0x00,0x00,0x00,0x00}};
+const uint8_t required[9][5] ={{0xEC,0x00,0x01,0x00,0xC3},{0x90,0x00,0x06,0x1F,0x0A},{0x80,0x00,0x00,0x00,0x03},{0x93,0x00,0x00,0x00,0x00},{0xF0,0x00,0x04,0x01,0xC8},{0x6F,0x00,0x00,0x00,0x00}};
 int QEM[16] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
-int target = 0;
-int buffer = 0;
-int target2 = 10000;
-int threshold = 25;
 uint8_t current=0;
 uint8_t previous=0;
-int count=0;
-//volatile int counts;
+volatile int count=0;
+volatile int steps1;
+volatile int steps2;
+bool toggle=true;
+bool isStepping;
+bool isStalled=false;
 bool direction;
 
 void standalone_mode(){
@@ -85,85 +85,96 @@ void spi_mode(){
 	}
 	return;
 }
+
+#define STEP_BUFFER_SIZE 5
+
+//volatile int8_t directionFilter[STEP_BUFFER_SIZE] = {0};
+//volatile int8_t currentDirectionIndex = 0;
+
 void EIC_Handler(void){
-	if(EIC->INTFLAG.reg & (EIC_INTFLAG_EXTINT4)|(EIC->INTFLAG.reg & EIC_INTFLAG_EXTINT5)){
+		if((EIC->INTFLAG.reg & EIC_INTFLAG_EXTINT5)||(EIC->INTFLAG.reg & EIC_INTFLAG_EXTINT4)){
 		uint32_t in = PORT->Group[0].IN.reg;
 		uint8_t bit1 = (bool)(in & PA04.bitmask); 
 		uint8_t bit2 = (bool)(in & PA05.bitmask);
 		previous=current;
 		current=2*bit1+bit2;
-		count+=QEM[previous*4+current];
-		if(count > 20000){count--;}
-		if(count < -1000){count++;}
+		int8_t currentDirection = QEM[previous*4+current];
+		/*if(currentDirection == 1 && direction && !isStalled){
+			isStalled=true;
+			direction=!direction;
+			//isStepping=false;
+		}
+		if(currentDirection == -1 && !direction && !isStalled){
+			isStalled=true;
+			direction=!direction;
+			//isStepping=false;
+		}*/
+		count += currentDirection;
 		EIC->INTFLAG.reg |= EIC_INTFLAG_EXTINT4| EIC_INTFLAG_EXTINT5;
 	}
-	}
-
-
-int main(void)
-{
-	count=0;
-	direction=false;
-	SYSCTRL->OSC8M.bit.PRESC = 0x0;//Making core clock 8MHz
-	initPins();
-	int counts=0;
-	int change = 5000;
+}
+void EIC_setup(void){
 	PM->APBAMASK.reg |= PM_APBAMASK_EIC;
 	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID_EIC | GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN(0);
-	PORT->Group[0].WRCONFIG.reg =  PORT_WRCONFIG_WRPINCFG|PORT_WRCONFIG_WRPMUX|PORT_WRCONFIG_INEN|PORT_WRCONFIG_PMUXEN|PORT_WRCONFIG_PMUX(MUX_PA04A_EIC_EXTINT4)|PORT_WRCONFIG_PINMASK((uint16_t)PA04.bitmask);
 	PORT->Group[0].WRCONFIG.reg =  PORT_WRCONFIG_WRPINCFG|PORT_WRCONFIG_WRPMUX|PORT_WRCONFIG_INEN|PORT_WRCONFIG_PMUXEN|PORT_WRCONFIG_PMUX(MUX_PA05A_EIC_EXTINT5)|PORT_WRCONFIG_PINMASK((uint16_t)PA05.bitmask);
-	EIC->EVCTRL.reg |=EIC_EVCTRL_EXTINTEO4 | EIC_EVCTRL_EXTINTEO5;
-	EIC->CONFIG[0].reg |= EIC_CONFIG_FILTEN4|EIC_CONFIG_SENSE4(0x3)|EIC_CONFIG_FILTEN5|EIC_CONFIG_SENSE5(0x3);
-	EIC->INTENSET.reg|= EIC_INTENSET_EXTINT4|EIC_INTENSET_EXTINT5;
+	PORT->Group[0].WRCONFIG.reg =  PORT_WRCONFIG_WRPINCFG|PORT_WRCONFIG_WRPMUX|PORT_WRCONFIG_INEN|PORT_WRCONFIG_PMUXEN|PORT_WRCONFIG_PMUX(MUX_PA04A_EIC_EXTINT4)|PORT_WRCONFIG_PINMASK((uint16_t)PA04.bitmask);
+	EIC->EVCTRL.reg |=EIC_EVCTRL_EXTINTEO5|EIC_EVCTRL_EXTINTEO4;
+	EIC->CONFIG[0].reg |= EIC_CONFIG_FILTEN5|EIC_CONFIG_SENSE5(0x3)|EIC_CONFIG_FILTEN4|EIC_CONFIG_SENSE4(0x3);
+	EIC->INTENSET.reg|= EIC_INTENSET_EXTINT5| EIC_INTENSET_EXTINT4;
 	EIC->CTRL.reg |=  EIC_CTRL_ENABLE;
 	NVIC_SetPriority(EIC_IRQn,0);
 	NVIC_EnableIRQ(EIC_IRQn);
 	__enable_irq();
-	unsigned long input=0;
-	setPin(LED, OUTPUT, NORMAL, PULL_DOWN);
-	writePin(LED,false);
+}
+int main(void)
+{
+	changeClock();
+	initPins();
+	EIC_setup();
 	initRTC();
 	standalone_mode();
-	//spi_mode();
-	bool toggle=true;
-	bool isStepping = true;
-	//previous = (readPin(&PA04))*2+ (readPin(&PA05));
-	while (1)
-    {	
-		if(isStepping){
-			delay_us(10);
-			writePin(STEP,toggle);
-			toggle=!toggle;
-		}
-		else
-		{
-			buffer=target;
-			target=target2;
-			target2=buffer;
-			isStepping=true;
-		}
-		if(count > target+threshold){writePin(DIRPIN,false);}
-		else if(count < target-threshold){writePin(DIRPIN,true);}
-		if(target-threshold < count){
-			isStepping=false;
-			}
-		if(count < target + threshold){
-			isStepping=false;
-			}
-		if(count >target+ threshold){isStepping=true;}
-		if(count<target-threshold){isStepping=true;}
-			/*
+	int counts=0;
+	int change = 4000;
+	count=0;
+	direction=false;
+	unsigned long input=0;
+	isStalled=false;
+	/*
+
+	isStepping=true;
+	direction = true;
+	steps1=0;
+	while(!isStalled){
 		writePin(STEP,toggle);
 		toggle = !toggle;
+		delay_us(15);
+		steps1++;
+	}
+	steps2=0;
+	delay_us(50);
+	isStalled=false;
+	isStepping=true;
+	direction=false;
+	while(!isStalled){
+		writePin(STEP,toggle);
+		toggle = !toggle;
+		delay_us(15);
+		steps2++;
+	}
+	*/
+	
+	while (1)
+    {
+		//if(!isStalled){
+		writePin(STEP,toggle);
+		toggle = !toggle;
+		delay_us(15);
 		counts++;
-		delay_us(100);
-		if(counts==change){
-			writePin(DIRPIN,direction);
-			spi_send_config(required[5],recv[5]);
-			spi_send_config(required[6],recv[6]);
+		if(counts>=change){
 			direction=!direction;
+			writePin(DIRPIN,direction);
 			counts=0;
-		}*/
-    }
-}
+		}
+		}
+	}
 
