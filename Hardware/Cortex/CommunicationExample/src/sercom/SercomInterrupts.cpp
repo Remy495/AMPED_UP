13681572d62debc3b84d16e92c886f3d63beb0a1
Casey Massar <caseymassar@gmail.com>
2021-04-17 15:43:26 -0400

    Fixed a bug where the I2C code for the cortex grabbed all of the Sercom interrupts

--- Hardware/Cortex/CommunicationExample/src/sercom/SercomInterrupts.cpp
+++ Hardware/Cortex/CommunicationExample/src/sercom/SercomInterrupts.cpp
@@ -5,20 +5,20 @@
 
 // Interrupt handlers for sercom units. Simply calls the application-supplied interrupt handler for the sercom in question
 
-void SERCOM0_Handler()
-{
-    SercomConfig<SercomHandle::SERCOM_0>::interruptHandler();
-}
+// void SERCOM0_Handler()
+// {
+//     SercomConfig<SercomHandle::SERCOM_0>::interruptHandler();
+// }
 
-void SERCOM1_Handler()
-{
-    SercomConfig<SercomHandle::SERCOM_1>::interruptHandler();
-}
+// void SERCOM1_Handler()
+// {
+//     SercomConfig<SercomHandle::SERCOM_1>::interruptHandler();
+// }
 
-void SERCOM2_Handler()
-{
-    SercomConfig<SercomHandle::SERCOM_2>::interruptHandler();
-}
+// void SERCOM2_Handler()
+// {
+//     SercomConfig<SercomHandle::SERCOM_2>::interruptHandler();
+// }
 
 void SERCOM3_Handler()
 {
