6faa2f3ef81c933112871a4d45ef66866ee8dd48
Casey Massar <caseymassar@gmail.com>
2021-04-27 20:52:35 -0400

    Reverted Amped-Up-DB-Control-Program to fix bug

---
+++ Hardware/Cortex/C/Amped-Up-DB-Control-Program/Amped-Up-DB-Control-Program/Amped-Up-DB-Control-Program/spi_control.h
@@ -0,0 +1,16 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 3/12/2021 11:43:13 AM
+ *  Author: hanle
+ */ 
+
+
+#ifndef SPI_CONTROL_H_
+#define SPI_CONTROL_H_
+//To Do: Create Send and Receive Methods
+
+
+
+
+#endif /* INCFILE1_H_ */
